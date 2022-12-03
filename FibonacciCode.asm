cf445b25e521e01ca0607625e682fdb805d5e536
jjackson15 <102105912+jjackson15@users.noreply.github.com>
2022-12-03 18:19:07 -0500

    Update FibonacciCode.asm
    moved input to register, called function, print prompt

--- FibonacciCode.asm
+++ FibonacciCode.asm
@@ -1,5 +1,6 @@
 .data
 integer: .asciiz "Enter a number greater than 25\n"
+prompt1: .asciiz "The Fibonnaci sequence is:\n"
 error: .aciiz "Illegal number!"
 
 .text
@@ -9,4 +10,12 @@ la $a0, integer
 syscall
 
 li $v0, 5
+syscall
+
+move $t0, $v0
+jal fibonnaci
+move $t1, $v0
+
+li $v0, 4
+la $a0, prompt1
 syscall
