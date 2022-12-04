c21dd936d28ec8dc0b5eeecdb966b7b6bcffa053
jjackson15 <102105912+jjackson15@users.noreply.github.com>
2022-12-04 16:23:23 -0500

    Update FibonacciCode.asm
    added space on stack, pushed to stack

--- FibonacciCode.asm
+++ FibonacciCode.asm
@@ -13,16 +13,17 @@ syscall
 li $v0, 5 #reads string
 syscall
 
-move $t0, $v0 #saves input to register
+move $a0, $v0 #saves input to register
+
 jal fibonnaci #calls function
-move $t1, $v0
+move $a1, $v0
 
 li $v0, 4 #prints second message 
 la $a0, prompt1
 syscall
 
 li $v0, 1 #prints result
-move $t0, $t1
+move $a0, $a               1
 syscall
 
 li $v0, 4
@@ -32,5 +33,14 @@ syscall
 
 li $v0, 10 #exits
 syscall
+
+fibonacci:
+addi $sp, $sp, -12
+sw $ra, 8($sp)
+sw $s0, 4($sp)
+sw $s1, 0($sp)
+move $s0, $a0
+
+
 
 
