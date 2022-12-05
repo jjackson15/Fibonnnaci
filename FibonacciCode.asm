1446ca2ce9f8944353cec8bb2dfe0c1f3205c185
jjackson15 <102105912+jjackson15@users.noreply.github.com>
2022-12-04 23:50:19 -0500

    Update FibonacciCode.asm

--- FibonacciCode.asm
+++ FibonacciCode.asm
@@ -1,20 +1,20 @@
 .data
-integer: .asciiz "Enter a number greater than 25\n"
+command: .asciiz "Enter a number greater than 25\n"
 prompt1: .asciiz "The Fibonnaci sequence is:\n"
 error: .asciiz "Illegal number!"
 comma: .asciiz ", "
-
+.globl main
 .text 
 main:
 li $v0, 4 #prints message 
-la $a0, integer 
+la $a0, command 
 syscall
 
 li $v0, 5 #reads string
 syscall
+beq $v0, 0, iszero
 
 move $a0, $v0 #saves input to register
-
 jal fibonnaci #calls function
 move $a1, $v0
 
@@ -34,28 +34,36 @@ syscall
 li $v0, 10 #exits
 syscall
 
-fibonacci:
-addi $sp, $sp, -12
-sw $ra, 8($sp)
-sw $s0, 4($sp)
-sw $s1, 0($sp)
-move $s0, $a0
+iszero:
+  li $v0, 4
+  la $a0, command
+  
+ islessthan:
+  li $v0, 4
+  la $a0, command
 
-li $v0, 1 # return value 
-ble $s0, 0x2, fibonacciExit 
-addi $a0, $s0, -1 
-jal fibonacci
-move $s1, $v0 # store result of f(n-1) to s1
-addi $a0, $s0, -2 
-jal fibonacci
-add $v0, $s1, $v0 # add f(n-1)
+fibonacci:
+  addi $sp, $sp, -12
+  sw $ra, 8($sp)
+  sw $s0, 4($sp)
+  sw $s1, 0($sp)
+  move $s0, $a0
+
+  li $v0, 1 # return value 
+  ble $s0, 0x2, fibonacciExit 
+  addi $a0, $s0, -1 
+  jal fibonacci
+  move $s1, $v0 # store result of f(n-1) to s1
+  addi $a0, $s0, -2 
+  jal fibonacci
+  add $v0, $s1, $v0 # add f(n-1)
 
 fibonacciExit:
-lw $ra, 8($sp)
-lw $s0, 4($sp)
-lw $s1, 0($sp)
-addi $sp, $sp, 12
-jr $ra
+  lw $ra, 8($sp)
+  lw $s0, 4($sp)
+  lw $s1, 0($sp)
+  addi $sp, $sp, 12
+  jr $ra
 
 
 
